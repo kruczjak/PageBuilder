d7be1bf5e6345f625ba022e655b7cd4fd321f8da
kruczjak <kruczjak@gmail.com>
2017-04-11 17:47:25 +0200

    Add devise gem

--- Gemfile
+++ Gemfile
@@ -22,8 +22,13 @@ gem 'puma', '~> 3.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# devise
+gem 'devise'
+gem 'devise_token_auth'
+gem 'omniauth-github'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :rememberable, :trackable, :omniauthable, omniauth_providers: [:github]
+end
